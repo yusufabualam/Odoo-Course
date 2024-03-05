333357c1d17616bcda3d9d2f02c2d1ab5ac774ed
Yusuf Abu-Alam <yabualam@gmail.com>
2024-03-05 23:06:48 +0200

    Lab 1

---
+++ todo_app/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Todo App",
+    'summary': """ """,
+    'author': "Yusuf Abu Alam",
+    'category': 'Productivity',
+    'version': '17.0.0.1.0',
+    'depends': ['base',
+                ],
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/base_menus.xml',
+        'views/ticket.xml',
+    ],
+}
